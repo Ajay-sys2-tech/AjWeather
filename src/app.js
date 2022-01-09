a22803324c6dd6540633069be4efb615fac8e517
Ajay-sys2-tech <ajaycu23@gmail.com>
2022-01-10 01:17:02 +0530

    after adding heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
-const port = 3300;
+const port = process.env.PORT || 3300;
 
 const path = require("path");
 
